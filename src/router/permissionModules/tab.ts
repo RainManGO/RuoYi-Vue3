c23e228ce5c4efa2d1762fc5b420f0d073650ada
王继民 <335768489@qq.com>
2021-04-12 11:50:44 +0800

    引入文件小写报错

--- src/router/permissionModules/tab.ts
+++ src/router/permissionModules/tab.ts
@@ -2,8 +2,8 @@
  * @Description:分栏
  * @Author: scy
  * @Date: 2021-01-08 19:21:46
- * @LastEditors: scy
- * @LastEditTime: 2021-01-21 21:22:52
+ * @LastEditors: WJM
+ * @LastEditTime: 2021-04-12 11:49:02
  */
 import { RouteRecordRaw } from 'vue-router'
 import Layout from '@/layout/Index.vue'
@@ -15,7 +15,7 @@ const tabRouter: Array<RouteRecordRaw> = [
     children: [
       {
         path: 'index',
-        component: () => import(/* webpackChunkName: "tab" */ '@/views/tab/index.vue'),
+        component: () => import(/* webpackChunkName: "tab" */ '@/views/tab/Index.vue'),
         name: 'Tab',
         meta: {
           title: 'tab',
